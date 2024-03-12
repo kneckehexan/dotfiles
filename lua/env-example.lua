f7c6781ba281a02bac96fef7aaeb989d22469bf0
Philip Tunbjer <philip.tunbjer@gmail.com>
2024-03-13 00:42:19 +0100

    chore: added example of env-file and updated gitignore accordingly

---
+++ lua/env-example.lua
@@ -0,0 +1,10 @@
+-- GOOGLE_* values are used for Calendar.vim. To obtain, follow instructions in ":h calendar" (https://github.com/itchyny/calendar.vim)
+
+local env = {
+	WIKI_PATH = "/path/to/path",
+	GOOGLE_API = "longapikey",
+	GOOGLE_CLIENT_ID = "longgoogleoauthclientid",
+	GOOGLE_CLIENT_SECRET = "secret",
+}
+
+return env
